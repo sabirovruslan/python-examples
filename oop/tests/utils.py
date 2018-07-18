6e64191118b3969ef1d1cf7bd110a485687ecdc2
Ruslan <sabirov@worksolutions.ru>
2018-07-19 00:04:54 +0300

    Testing. Доработка метода циклического прогона кейсов

--- oop/tests/utils.py
+++ oop/tests/utils.py
@@ -4,12 +4,12 @@ from functools import wraps
 def cases(items):
     def decorator(f):
         @wraps(f)
-        def wrapper(*args):
+        def wrapper(self, *args):
             for item in items:
                 new_args = args + (item if isinstance(item, tuple) else (item,))
-                try:
-                    f(*new_args)
-                except AssertionError as e:
-                    raise AssertionError(e.args + (f'case: {new_args}',))
+                with self.subTest(f'case: {new_args}'):
+                    f(self, *new_args)
+
         return wrapper
+
     return decorator
