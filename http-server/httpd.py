e53a5ad98162c69df594d30c25268b1421612e16
Ruslan <sabirov@worksolutions.ru>
2018-07-26 01:30:19 +0300

    Http server. Рефакторинг

--- http-server/httpd.py
+++ http-server/httpd.py
@@ -7,50 +7,29 @@ from datetime import datetime
 from multiprocessing import Process
 from urllib import parse, request
 
-DEFAULT_HOST = 'localhost'
-DEFAULT_PORT = 80
-AVAILABLE_METHODS = ['GET', 'HEAD']
-DEFAULT_BUFFER_SIZE = 1024
-
 OK = 200
 BAD_REQUEST = 400
 FORBIDDEN = 403
 NOT_FOUND = 404
 NOT_ALLOWED = 405
 
-class BadRequestException(Exception):
-    pass
 
-class ForbiddenException(Exception):
-    pass
+class RequestHandler:
+    AVAILABLE_METHODS = ['GET', 'HEAD']
+    DEFAULT_BUFFER_SIZE = 1024
 
-class HttpServer:
+    def __init__(self, sock):
+        self.sock = sock
 
-    def __init__(self, **kwargs):
-        self.hostname = kwargs.get('hostname', DEFAULT_HOST)
-        self.port = kwargs.get('port', DEFAULT_PORT)
-        self.document_root = kwargs.get('document_root')
-        self.sock = self._init_sock()
-
-    def _init_sock(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind((self.hostname, self.port))
-        sock.listen(1)
-        logging.info('Server start')
-
-        return sock
-
-    def wait_for(self):
-        logging.info('Start worker: {}'.format(os.getpid()))
-        while True:
-            sock, address = self.sock.accept()
-            data = sock.recv(DEFAULT_BUFFER_SIZE)
-            body = ''
+    def handle_request(self):
+        data = self.sock.recv(self.DEFAULT_BUFFER_SIZE)
+        data = data.decode('utf-8')
+        method = data.split(' ')[0]
+        body = ''
+        if method not in self.AVAILABLE_METHODS:
+            response = self._create_headers(BAD_REQUEST)
+        else:
             try:
-                data = data.decode('utf-8')
-                method = data.split(' ')[0]
-                if method not in AVAILABLE_METHODS:
-                    raise BadRequestException('Not available method')
                 path_string = data.split(' ')[1]
                 path = self._parse_path(path_string)
                 if method == 'GET':
@@ -59,25 +38,33 @@ class HttpServer:
                 response = self._create_headers(OK, path)
             except (FileNotFoundError, NotADirectoryError):
                 response = self._create_headers(NOT_FOUND)
-            except BadRequestException:
-                response = self._create_headers(BAD_REQUEST)
-            except ForbiddenException:
-                response = self._create_headers(FORBIDDEN)
-            except Exception:
-                response = self._create_headers(NOT_ALLOWED)
-            if body:
-                response += body
-            sock.send(response)
-            sock.close()
+        if body:
+            response += body
+        self.sock.send(response)
+        self.sock.close()
 
     def _parse_path(self, path_string):
         path_unquoted = parse.unquote(path_string)
         path_wo_args = path_unquoted.split('?', 1)[0]
+        path_wo_args = self._remove_dot_segments(path_wo_args)
         if path_wo_args.endswith('/'):
             path_wo_args += 'index.html'
-        path = os.path.join(self.document_root, *path_wo_args.split('/'))
-        if '/..' in path:
-            raise ForbiddenException('Access denied')
+        path = os.path.join(DOCUMENT_ROOT, *path_wo_args.split('/'))
+        return path
+
+    @staticmethod
+    def _remove_dot_segments(path):
+        if path.startswith('.'):
+            path = '/' + path
+        while '../' in path:
+            p1 = path.find('/..')
+            p2 = path.rfind('/', 0, p1)
+            if p2 != -1:
+                path = path[:p2] + path[p1 + 3:]
+            else:
+                path = path.replace('/..', '', 1)
+        path = path.replace('/./', '/')
+        path = path.replace('/.', '')
         return path
 
     def _create_headers(self, code, path=''):
@@ -101,6 +88,30 @@ class HttpServer:
         return h.encode('utf-8')
 
 
+class HttpServer:
+    DEFAULT_LISTEN_BACKLOG = 10
+
+    def __init__(self, hostname='localhost', port=80):
+        self.hostname = hostname
+        self.port = port
+        self.sock = self._init_sock()
+
+    def _init_sock(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((self.hostname, self.port))
+        sock.listen(self.DEFAULT_LISTEN_BACKLOG)
+        logging.info('Server start')
+
+        return sock
+
+    def serve_forever(self):
+        logging.info('Start worker: {}'.format(os.getpid()))
+        while True:
+            sock, address = self.sock.accept()
+            handler = RequestHandler(sock)
+            handler.handle_request()
+
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-w', help='Number of workers', default=4)
@@ -113,14 +124,15 @@ if __name__ == '__main__':
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
+    DOCUMENT_ROOT = os.path.abspath(args.r)
+
     server = HttpServer(
         hostname=os.environ.get('HOSTNAME'),
-        document_root=os.path.abspath(args.r)
     )
 
     process_list = []
     for _ in range(int(args.w)):
-        p = Process(target=server.wait_for)
+        p = Process(target=server.serve_forever)
         p.daemon = True
         p.start()
         process_list.append(p)
