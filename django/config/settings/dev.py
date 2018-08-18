dcf02ffdce209ff8838c2f1506dfc0a80fd1856c
Ruslan <sabirov@worksolutions.ru>
2018-08-18 13:13:22 +0300

    Django. Доработка окружения

--- django/config/settings/dev.py
+++ django/config/settings/dev.py
@@ -1,3 +1,16 @@
 from .base import *
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_DATABASE'),
+        'USER': os.environ.get('DB_USERNAME'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PORT': os.environ.get('DB_PORT'),
+        'HOST': os.environ.get('DB_HOST'),
+    }
+}
