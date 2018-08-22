fdd0db348253237a27cd39eeb3a09f6626a1c193
Ruslan <sabirov@worksolutions.ru>
2018-08-23 01:20:00 +0300

    Django. Регистрация пользователя

--- django/config/settings/base.py
+++ django/config/settings/base.py
@@ -91,3 +91,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'config', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
