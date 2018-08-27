4edba9e29d49676b10f7ff9da16dc7c9236894ef
Ruslan <sabirov@worksolutions.ru>
2018-08-27 23:07:22 +0300

    Django. Добавление ответа

--- django/stackoverflow/urls.py
+++ django/stackoverflow/urls.py
@@ -5,7 +5,6 @@ from .views import QuestionCreateView, TagListView, QuestionDetailView, AnswerCr
 urlpatterns = [
     url(r'^question/create', QuestionCreateView.as_view(), name='create_question'),
     url(r'^question/(?P<question_id>[0-9]*)$', QuestionDetailView.as_view(), name='question_detail'),
-    url(r'^question/(?P<question_id>[0-9]*)/answers$', AnswerCreateView.as_view(), name="new_answer"
-    ),
+    url(r'^question/(?P<question_id>[0-9]*)/answers$', AnswerCreateView.as_view(), name='new_answer'),
     url(r'^tags$', TagListView.as_view(), name='tags_list'),
 ]
