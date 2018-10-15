ffc2a2ea24573b4383241254bc820676c6855992
Ruslan <sabirov@worksolutions.ru>
2018-10-15 20:32:29 +0300

    goland. Тест каналов

--- golang/memc_load.go
+++ golang/memc_load.go
@@ -9,14 +9,14 @@ func printCount(c chan int) {
 	v := 0
 	for v >=0 {
 		v = <-c
-		fmt.Println(v, "")
+		fmt.Println("Чтение из канала: ", v)
 	}
 }
 
 func main() {
 	ch := make(chan int)
 	go printCount(ch)
-	a :=[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1}
+	a :=[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2}
 	for _, i := range a {
 		ch<-i
 	}
